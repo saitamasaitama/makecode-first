fcd1f55d719025cc0c6bec50d814c851c91a60e0
president saitama-tec inc <30422219+saitamasaitama@users.noreply.github.com>
2020-02-06 03:23:46 +0900

    Update pxt.json, main.blocks, main.ts, images.ts

--- main.ts
+++ main.ts
@@ -1,71 +1,84 @@
-
-let pos = {x:0,y:0};
-const sprite= sprites.create(img`
-    cccccccc
-    11111111
-    22222222
-    33333333
-    44444444
-    55555555
-    66666666
-    77777777
-    88888888
-`);
-sprite.x=100;
-sprite.y=20;
-sprite.say("こんにちは")
-
-scene.centerCameraAt(0,60)
-
-let index=0;
-const photos=[
-    Assets.Images.IMAGE_2,
-    Assets.Images.IMAGE_3,
-    Assets.Images.IMAGE_NAME,
-];
-
-image.setPalette(Assets.Images.IMAGE_2.palette)
-//scene.setBackgroundImage(Assets.Images.IMAGE_2.image)
-
-scene.addBackgroundLayer(Assets.Images.IMAGE_2.image)
-
-function imageChange(index:number){
-//    scene.setBackgroundImage(photos[index].image)
+controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
+    index += 1
+    index = index % 3
+//    imageChange(index)
+    // scene.centerCameraAt(pos.x, pos.y)
+    // scene.backgroundImage().scroll(0, -10)
+    sprite.say(`POS=${sprite.x} ${sprite.y}`)
+})
+controller.player1.onButtonEvent(ControllerButton.Down, ControllerButtonEvent.Pressed, function () {
+	
+})
+/*
+function imageChange (index: number) {
+    scene.setBackgroundImage(img`
+        a 6 e 4 a e . a 7 e
+    `)
     image.setPalette(photos[index].palette)
 }
+*/
+controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+})
+controller.player1.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
 
-controller.player1.onButtonEvent(
-    ControllerButton.Right,
-    ControllerButtonEvent.Pressed,()=>{
-        index++
-        index=(index % 3 )
-        imageChange(index)
-        pos.x+=10;        
-        scene.centerCameraAt(pos.x, pos.y)
-        //scene.backgroundImage().scroll(0, -10)
-        sprite.say(`INDEX=${index}`)
 })
 
-controller.player1.onButtonEvent(
-    ControllerButton.Left,
-    ControllerButtonEvent.Pressed, () => {
-        index--
-        if(index<0)index=2
-        index = (index % 3)
-        sprite.say(`INDEX=${index}`)
-        pos.x -= 10;        
-        scene.centerCameraAt(pos.x,pos.y)
-        imageChange(index)
-        //scene.backgroundImage().scroll(0, 10)
-        
-    })
 
-controller.player1.onButtonEvent(
-    ControllerButton.Up,
-    ControllerButtonEvent.Pressed, () => scene.backgroundImage().scroll(0, 10)
-);
+interface palettedimage{
+    image: Image,
+    palette:Buffer
+}
 
-controller.player1.onButtonEvent(
-    ControllerButton.Down,
-    ControllerButtonEvent.Pressed, () => scene.backgroundImage().scroll(0, -10)
-);
+
+const phots:palettedimage[]=[
+    Assets.Images.IMAGE_1,
+    Assets.Images.IMAGE_2,
+    Assets.Images.IMAGE_3,
+];
+
+let sprite: Sprite =sprites.create(Assets.Images.BPImage.image);
+image.setPalette(Assets.Images.BPImage.palette)
+let index=0
+let pos = {x:0,y:0};
+let arrow= sprites.create(img`
+    c c c c c c c c
+    1 1 1 . . 1 1 1
+    2 2 2 . . 2 2 2
+    3 3 3 . . 3 3 3
+    4 4 4 4 4 4 4 4
+    5 5 5 f f 5 5 5
+    6 6 6 f f 6 6 6
+    7 7 7 f f 7 7 7
+    8 8 8 f f 8 8 8
+`, 0)
+sprite.x = sprite.width >> 1;
+sprite.y = sprite.height >> 1;
+//arrow.say("こんにちは")
+// color.setPalette(palette) scene.centerCameraAt(0,0)
+sprite.z = 0
+
+game.onUpdate(()=>{
+    index++;
+    index=index%3
+    //sprite.setImage(img)
+    sprite = sprites.create(phots[index].image);
+    
+})
+
+
+controller.moveSprite(arrow)
+scene.cameraFollowSprite(arrow)
+scene.setTileMap(img`
+    . . . . . . . . . . . . . . . . . . . .
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . .
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . . .
+    1 1 1 1 1 1 1 1 1 1 1 . . . . . . . . .
+    2 2 2 2 2 2 2 2 2 2 2 . . . . . . . . .
+    3 3 3 3 3 3 3 3 3 3 3 . . . . . . . . .
+    1 1 1 1 1 1 1 1 1 1 1 1 1 . . . . . . .
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 . . . . .
+    3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3
+    4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 . . . . .
+    9 9 9 9 9 9 9 9 9 9 9 9 . . . . . . . .
+    . . . . . . . . . . . . . . . . . . . .
+`, TileScale.ThirtyTwo)
